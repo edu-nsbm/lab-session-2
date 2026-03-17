7ce9ad276d344c6fd94a511394c56cfe735d0ee0
EDU NSBM <edunsbm@gmail.com>
2026-03-17 14:58:45 +0530

    Add question 4

---
+++ question4.py
@@ -0,0 +1,17 @@
+def main() -> None:
+    length: float = 10.0
+    width: float = 5.0
+    area: float = width * length
+
+    print(
+        "The area of a rectangle with length "
+        + str(length)
+        + " and width "
+        + str(width)
+        + " is "
+        + str(area)
+    )
+
+
+if __name__ == "__main__":
+    main()
